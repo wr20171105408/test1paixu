3dd0d8a66e9821e3efb42291ff7f6688478fc21e
s20171105408 <s20171105408@admindeimac.local>
2018-09-28 20:10:42 +0800

    Initial Commit

---
+++ test1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test1
+//
+//  Created by s20171105408 on 2018/9/28.
+//  Copyright © 2018年 s20171105408. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
